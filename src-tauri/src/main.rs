1a33850886ff5d5ad2218d8e34e3ca37825bb7c0
Sir-Thom <Sir-Thom1702@proton.me>
2023-07-12 12:23:40 -0400

    Make the the shell be dynamics  size  to window
    Added the function `async_create_shell which create the shell it that process if the user have set it .bashrc  neofetch it will be size correctly. Also added  to the shell TERM so in build you can use cls or clear. Finally I have updated tauri  and other packages.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,48 @@
 use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
 use std::{
     io::{BufRead, BufReader, Write},
+    process::exit,
     sync::{Arc, Mutex},
     thread::{self, sleep},
     time::Duration,
 };
+
 use tauri::{async_runtime::Mutex as AsyncMutex, State};
 
 struct AppState {
     pty_pair: Arc<AsyncMutex<PtyPair>>,
     writer: Arc<AsyncMutex<Box<dyn Write + Send>>>,
+}
+#[tauri::command]
+// create a shell and add to it the $TERM env variable so we can use clear and other commands
+async fn async_create_shell(state: State<'_, AppState>) -> Result<(), String> {
+    #[cfg(target_os = "windows")]
+    let mut cmd = CommandBuilder::new("powershell.exe");
+
+    #[cfg(not(target_os = "windows"))]
+    let mut cmd = CommandBuilder::new("bash");
+
+    // add the $TERM env variable so we can use clear and other commands
+
+    #[cfg(target_os = "windows")]
+    cmd.env("TERM", "cygwin");
+
+    #[cfg(not(target_os = "windows"))]
+    cmd.env("TERM", "xterm-256color");
+
+    let mut child = state
+        .pty_pair
+        .lock()
+        .await
+        .slave
+        .spawn_command(cmd)
+        .map_err(|err| err.to_string())?;
+
+    thread::spawn(move || {
+        let status = child.wait().unwrap();
+        exit(status.exit_code() as i32)
+    });
+    Ok(())
 }
 
 #[tauri::command]
@@ -47,17 +80,6 @@ fn main() {
         })
         .unwrap();
 
-    #[cfg(target_os = "windows")]
-    let cmd = CommandBuilder::new("powershell.exe");
-    #[cfg(not(target_os = "windows"))]
-    let cmd = CommandBuilder::new("bash");
-
-    let mut child = pty_pair.slave.spawn_command(cmd).unwrap();
-
-    thread::spawn(move || {
-        child.wait().unwrap();
-    });
-
     let reader = pty_pair.master.try_clone_reader().unwrap();
     let writer = pty_pair.master.take_writer().unwrap();
 
@@ -88,7 +110,8 @@ fn main() {
         })
         .invoke_handler(tauri::generate_handler![
             async_write_to_pty,
-            async_resize_pty
+            async_resize_pty,
+            async_create_shell
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
